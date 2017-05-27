95f9fdb3b6711cd828fdeede8def1a21cb866248
王鑫 <wangxin@souche.com>
2017-05-27 16:08:42 +0800

    change to jitpack

--- app/src/main/java/wang/relish/ugo/sample/MainActivity.java
+++ app/src/main/java/wang/relish/ugo/sample/MainActivity.java
@@ -3,6 +3,8 @@ package wang.relish.ugo.sample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
 import wang.relish.ugo.R;
 
 public class MainActivity extends AppCompatActivity {
